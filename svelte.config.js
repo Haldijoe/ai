e906ce13384f2fcdf51c99c23aaf4b84166394e1
Haldijoe <haldijoe@students.zhaw.ch>
2024-05-24 08:39:29 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+export default {
+  preprocess: preprocess(),
+  kit: {
+    target: '#svelte',
+    adapter: adapter(),
+    vite: {
+      ssr: {
+        noExternal: ['@sveltejs/kit']
+      }
+    }
+  }
 };
-
-export default config;
